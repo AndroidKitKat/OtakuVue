7ff527e6578986f5cf41c8b4af9d6404591da526
Jason Witt <jawittdesigns@gmail.com>
2018-08-20 08:12:29 -0400

    Added MainNave.vue component

---
+++ src/components/MainNav.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav id="header__nav" class="heacer__nav header" role="navigation">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'mainNav',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  // Lifecycle Hooks
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+}
+</script>
