52e0a0184f59807f8d445ac464140956e7167e19
Jason Witt <jawittdesigns@gmail.com>
2018-08-20 17:39:59 -0400

    Updated jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,21 @@
 module.exports = {
-  moduleFileExtensions: ["js", "jsx", "json", "vue"],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
   transform: {
-    "^.+\\.vue$": "vue-jest",
-    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
-      "jest-transform-stub",
-    "^.+\\.jsx?$": "babel-jest"
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.jsx?$': 'babel-jest',
   },
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1"
-  },
-  snapshotSerializers: ["jest-serializer-vue"],
-  testMatch: [
-    "**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
+  moduleNameMapper: require('./aliases.config').jest,
+  snapshotSerializers: ['jest-serializer-vue'],
+  testPathIgnorePatterns: ['.eslintrc.js'],
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!**/node_modules/**',
+    '!src/main.js',
+    '!src/app.vue',
+    '!src/router/**.js',
+    '!src/store/**.js',
   ],
-  testURL: "http://localhost/"
-};
+  testMatch: ['**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'],
+  testURL: 'http://localhost/',
+}
