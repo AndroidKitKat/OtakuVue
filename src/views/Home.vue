4a42c18f6342fe00daf45ec9c236a63e41570e14
Jason Witt <jawittdesigns@gmail.com>
2018-08-24 14:08:03 -0400

    Removed Home.vue view excess

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,34 +3,3 @@
     <p>Home</p>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'home',
-  components: {},
-  props: {},
-  data() {
-    return {
-      // Add to Header component
-      // :attr="headerAttrs"
-      headerAttrs: {
-        id: '',
-        class: '',
-        attrs: '',
-      },
-    }
-  },
-  methods: {},
-  computed: {},
-  watch: {},
-  // Lifecycle Hooks
-  beforeCreate() {},
-  created() {},
-  beforeMount() {},
-  mounted() {},
-  beforeUpdate() {},
-  updated() {},
-  beforeDestroy() {},
-  destroyed() {},
-}
-</script>
