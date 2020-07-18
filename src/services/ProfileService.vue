0fe5aee56171e0fd4b8b221101fc269b21f1d5a1
Mike E <kitkat@hey.com>
2020-07-17 22:31:07 -0400

    Add profile page

--- src/services/ProfileService.vue
+++ src/services/ProfileService.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="ProfileService" class="col-md-4 mx-auto">
-    succ
+    <h1 class="text-center my-3">Profile</h1>
+    <button class="btn btn-outline-danger" @click="logUserOut">Log Out</button>
   </div>
 </template>
 
@@ -14,6 +15,10 @@ export default {
     return {}
   },
   methods: {
+    logUserOut: function() {
+      Cookies.remove('id')
+      $(location).attr('href', '/')
+    },
     checkLogin: function() {
       var cookie = Cookies.get('id')
       // redirect users who aren't logged in
